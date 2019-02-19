39d69ebc7f369e057b25a9fd5a62b2eaf2fb5375
alediwait <45946482+alediwait@users.noreply.github.com>
2019-02-19 09:14:47 -0300

    Create AutoConDiwaIT.cmd
    Este Script esta en desarrollo. Por ahora funcionan todos los menues pero el unico que genera el archivo DOCKERFILE es la opcion NODEJS usando el LATEST.

---
+++ AutoConDiwaIT.cmd
@@ -0,0 +1,170 @@
+@echo off
+cls
+:arranq
+color a
+cls
+title Creador de Imagenes de Docker 2019 - DiwaIT
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo     ** Bienvenido al creador automatico de Imagenes de Docker - 2019 DiwaIT **
+echo.
+echo.
+echo     En breves selecciones podremos crear un dockerfile a medida.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo     ** Bienvenido al creador automatico de Imagenes de Docker - 2019 DiwaIT **
+echo.
+echo.
+echo  USO DESEADO=
+echo     1- NodeJS
+echo     2- LAMP
+echo     3- Jenkins
+echo     4- Blockchain
+echo     5- Salir del programa
+echo.
+echo.
+echo.
+echo.
+choice /C 12345 /M ">> "
+if errorlevel 255 goto final
+if errorlevel 5 goto final
+if errorlevel 4 goto blok
+if errorlevel 3 goto jen
+if errorlevel 2 goto lamp
+if errorlevel 1 goto node
+if errorlevel 0 goto final
+
+pause
+
+
+:blok
+
+echo Seccion blockchain de Docker a Construir
+pause
+exit
+
+:jen
+
+echo Seccion blockchain de Docker a Construir
+pause
+exit
+
+
+:lamp
+
+echo Seccion blockchain de Docker a Construir
+pause
+exit
+
+
+:node
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo     ** NODEJS - Custom para DiwaIT 2019 **
+echo.
+echo.
+echo.    
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo     ** NODEJS - Custom para DiwaIT 2019 **
+echo.
+echo.
+echo     Eleji la version=
+echo     1- NodeJS-LATEST
+echo     2- NODEJS Custom (hay que saber el tag)
+echo     3- Volver al menu principal
+echo.
+echo.
+echo.
+echo.
+choice /C 123 /M ">> "
+if errorlevel 255 goto final
+if errorlevel 3 goto arranq
+if errorlevel 2 goto cust
+if errorlevel 1 goto latest
+if errorlevel 0 goto final
+
+
+:cust
+
+:latest
+cls
+echo.
+echo.
+echo  ESTAMOS GENERANDO EL DOCKERFILE
+echo FROM node:latest >dockerfile
+echo. >>dockerfile
+echo MAINTAINER diwait alejandro@diwait.com >>dockerfile
+echo. >>dockerfile
+echo RUN apt-get update >>dockerfile
+echo. >>dockerfile
+echo EXPOSE 80 >>dockerfile
+echo. >>dockerfile
+echo RUN mkdir /root/nodeapp >>dockerfile
+echo. >>dockerfile
+echo WORKDIR /root/nodeapp >>dockerfile
+echo. >>dockerfile
+echo.
+ping localhost >nul
+cls
+echo.
+echo.
+echo     DOCKERFILE CREADO!
+echo.
+echo.
+ping localhost >nul
+goto final
+
+
+
+
+
+
+
+
+
+
+:final
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo Gracias por usar el programa!
+echo.
+echo.
+ping localhost >nul
+exit
+
+
+
+
